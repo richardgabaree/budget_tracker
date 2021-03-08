397216130bd89e38859210cf5dc0fec92f07f103
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-07 20:48:57 -0600

    added activate to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-
+// install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -20,4 +20,24 @@ self.addEventListener("install", function (evt) {
   );
 
   self.skipWaiting();
+});
+
+//activate
+
+// activate
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
